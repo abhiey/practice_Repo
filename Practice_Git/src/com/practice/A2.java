e7ffd3c8f3ac510d1cd3e26cef4eef8e60337e27
abhijeet <abhikrsingh90976@gmail.com>
2025-02-17 12:11:14 +0530

    ravi

--- Practice_Git/src/com/practice/A2.java
+++ Practice_Git/src/com/practice/A2.java
@@ -2,7 +2,7 @@ package com.practice;
 
 public class A2 {
 public static void main(String[] args) {
-	System.out.println("chssaxnsssssgeddsssssd");
+	System.out.println("cssshssaxnsssssgeddsssssd");
 	
 }
 }
